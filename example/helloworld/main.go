18643e5f0c4307989b6f285bc0388c41ff521d9c
mrchar <mrchar@qq.com>
2021-04-22 09:38:12 +0800

    添加Pluck和Respond函数

--- example/helloworld/main.go
+++ example/helloworld/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/mrchar/hraf"
-	"github.com/mrchar/hraf/implement/codec/json"
 )
 
 func main() {
@@ -18,15 +17,24 @@ func main() {
 	}
 }
 
+type greetParams struct {
+	Name string `json:"name"`
+}
+
 type greetResp struct {
 	Message string `json:"message"`
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	buffer, err := json.Encode(greetResp{"hello world!"})
-	if err != nil {
+	var params greetParams
+
+	if err := hraf.Pluck(r, &params); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := hraf.Respond(w, greetResp{"hello " + params.Name + "!"}); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.Write(buffer)
 }
--- hraf.go
+++ hraf.go
@@ -1,7 +1,11 @@
 package hraf
 
 import (
+	"io/ioutil"
+	"net/http"
+
 	def "github.com/mrchar/hraf/definition"
+	"github.com/mrchar/hraf/implement/codec/json"
 	"github.com/mrchar/hraf/implement/router"
 	"github.com/mrchar/hraf/implement/server"
 )
@@ -12,4 +16,28 @@ func New(address string, router def.Router) def.Server {
 
 func Default(address string) def.Server {
 	return server.New(address, router.New())
+}
+
+func Pluck(r *http.Request, v interface{}) error {
+	buffer, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	if err = json.Decode(buffer, v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Respond(w http.ResponseWriter, v interface{}) error {
+	w.WriteHeader(http.StatusOK)
+
+	buffer, err := json.Encode(v)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buffer)
+	return err
 }
